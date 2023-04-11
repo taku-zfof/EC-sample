2b71d34895c83f70bd57693e7de31dabfd043595
EC2 Default User <taku.uchida.zfof@gmail.com>
2023-04-11 23:59:59 +0900

    deviseのユーザーごとの遷移先分岐、表示分岐

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root to:'homes#top'
+  devise_for :users
+
+  get "homes/master"=>"homes#master" ,as: "master"
+  get "homes/user"=>"homes#user", as: "user"
+  get "homes/example"=>"homes#example" , as: "example"
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
