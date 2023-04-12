8ce1f122e4b32707c198ea4fdb3596a39c7de3dc
EC2 Default User <taku.uchida.zfof@gmail.com>
2023-04-12 01:36:01 +0000

    遷移先分岐、表示分岐のテスト・修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,16 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if current_user.email=="master@sample"
      master_path
-   else
+    else
      user_path
-   end
+    end
   end
-  
-  
+
+
 
   protected
+
+  def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+ end
 end
